da180c986d1a8568c6bdf58fd938e549865fdc1d
NikaIsaiashvili <nisaiashvili@gmail.com>
2022-04-18 01:36:28 +0400

    ex5

---
+++ ex5.js
@@ -0,0 +1,30 @@
+const randomUserAPI = "https://randomuser.me/api";
+
+const fetchBtn = document.querySelector("#fetch");
+const mainDiv = document.querySelector(".section-1");
+const outputDiv = document.querySelector(".outputDiv");
+const usrImg = document.querySelector(".usrImg");
+const usrEmail = document.querySelector(".usrEmail");
+const userLocation = document.querySelector(".userLocation");
+const userNames = document.querySelector(".userNames");
+
+
+function getUser() {
+fetch(randomUserAPI)
+.then(response => response.json())
+.then(function result(data) {
+    const randomUser = data.results[0];
+    const {picture: {medium}, email, location: {city}, name: {title, first, last} } = randomUser;
+    
+    usrImg.setAttribute('src', medium);
+    
+    usrEmail.textContent = email;
+    
+    userLocation.textContent = city;
+
+    userNames.textContent = `${title} ` + `${first} ` + `${last}`;
+
+});
+}
+
+fetchBtn.addEventListener('click', getUser);
